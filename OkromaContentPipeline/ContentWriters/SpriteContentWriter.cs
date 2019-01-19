be07a2de90a2a6f720609ec3033b25c59992a56e
Timothy Ji <JiTimothy@outlook.com>
2019-01-19 16:22:18 -0500

    added sprite and rectangle content writers.

---
+++ OkromaContentPipeline/ContentWriters/SpriteContentWriter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework.Content.Pipeline;
+using Microsoft.Xna.Framework.Content.Pipeline.Serialization.Compiler;
+
+namespace OkromaContentPipeline.ContentWriters
+{
+    [ContentTypeWriter]
+    public class SpriteContentWriter : ContentTypeWriter<Sprite>
+    {
+        public override string GetRuntimeReader(TargetPlatform targetPlatform)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void Write(ContentWriter output, Sprite value)
+        {
+            output.Write(value.TexturePath);
+            output.WriteObject(value.SourceRectangle, new XnaRectangleContentWriter());
+            output.Write(value.Origin);
+        }
+    }
+}
