f74331c780cee8799d638858bf3ae85b1f95086e
Timothy Ji <JiTimothy@outlook.com>
2019-01-20 16:32:05 -0500

    Added Tileset system.

---
+++ OkromaContentPipeline/TilesetPipeline/TilesetProcessor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework.Content.Pipeline;
+using System.Collections.Generic;
+
+namespace OkromaContentPipeline.TilesetPipeline
+{
+    [ContentProcessor(DisplayName = "Tileset -- " + nameof(OkromaContentPipeline))]
+    public class TilesetProcessor : ContentProcessor<Dictionary<string, string>, TilesetData>
+    {
+        public override TilesetData Process(Dictionary<string, string> input, ContentProcessorContext context)
+        {
+            var data = new TilesetData(input.Count);
+            foreach (var item in input)
+            {
+                string hex = item.Key;
+                string tilePath = item.Value;
+
+                //RGBA Hex to ARGB uint.
+                var r = uint.Parse(hex.Substring(0, 2), System.Globalization.NumberStyles.HexNumber);
+                var g = uint.Parse(hex.Substring(2, 2), System.Globalization.NumberStyles.HexNumber);
+                var b = uint.Parse(hex.Substring(4, 2), System.Globalization.NumberStyles.HexNumber);
+                var alpha = uint.Parse(hex.Substring(6, 2), System.Globalization.NumberStyles.HexNumber);
+
+                uint packedColorValue = (alpha << 24) | (b << 16) | (g << 8) | r;
+                data.AddColorTilePair(packedColorValue, tilePath);
+            }
+            return data;
+        }
+    }
+}
---
+++ OkromaContentPipeline/TilesetPipeline/TilesetWriter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework.Content.Pipeline;
+using Microsoft.Xna.Framework.Content.Pipeline.Serialization.Compiler;
+
+namespace OkromaContentPipeline.TilesetPipeline
+{
+    [ContentTypeWriter]
+    public class TilesetWriter : ContentTypeWriter<TilesetData>
+    {
+        public override string GetRuntimeReader(TargetPlatform targetPlatform)
+        {
+            return "Okroma.ContentReaders.TilesetContentReader, Okroma";
+        }
+
+        protected override void Write(ContentWriter output, TilesetData value)
+        {
+            output.Write(value.ColorTilePairCount);
+            foreach (var pair in value.GetColorTilePairs())
+            {
+                output.Write(pair.Key);
+                output.Write(pair.Value);
+            }
+        }
+    }
+}
