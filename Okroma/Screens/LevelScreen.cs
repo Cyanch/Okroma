b2261dc65d5c99e14fbf298aaea3c170d8a8c619
Timothy Ji <JiTimothy@outlook.com>
2019-01-20 17:28:08 -0500

    prep-screen class for levels.

---
+++ Okroma/Screens/LevelScreen.cs
@@ -0,0 +1,19 @@
+﻿using Okroma.Cameras;
+using Okroma.Physics;
+using Okroma.World;
+
+namespace Okroma.Screens
+{
+    public class LevelScreen : GameScreen
+    {
+        Player player;
+        Camera camera;
+
+        World2D world;
+        ICollidableSource collidables;
+
+        protected override void Initialize()
+        {
+        }
+    }
+}
