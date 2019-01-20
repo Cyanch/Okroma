3c281a323fc83a9c52b7f18ae2b46b8221a88a17
Timothy Ji <JiTimothy@outlook.com>
2019-01-20 17:56:53 -0500

    expanded levelscreen.cs

--- Okroma/Screens/LevelScreen.cs
+++ Okroma/Screens/LevelScreen.cs
@@ -1,4 +1,7 @@
-﻿using Okroma.Cameras;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Okroma.Cameras;
 using Okroma.Physics;
 using Okroma.World;
 
@@ -9,11 +12,41 @@ namespace Okroma.Screens
         Player player;
         Camera camera;
 
+        const int chunkSize = 8;
         World2D world;
         ICollidableSource collidables;
 
         protected override void Initialize()
         {
+            var collidableSourceCollection = new CollidableSourceCollection();
+            collidables = collidableSourceCollection;
+            collidableSourceCollection.AddSingle(player);
+            collidableSourceCollection.AddSource(world);
+        }
+
+        public override void LoadContent(ContentManager content)
+        {
+            base.LoadContent(content);
+            player = new Player("Player", null, Transform2D.None, collidables);
+        }
+
+        public override void Update(GameTime gameTime, IGameScreenInfo info)
+        {
+            base.Update(gameTime, info);
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+            {
+                player.Draw(gameTime, spriteBatch, world.LayerToDepth((int)Layers.Main));
+                world.Draw(gameTime, spriteBatch, camera.ViewRectangle);
+            }
+        }
+
+        public enum Layers
+        {
+            Main = 0
         }
     }
 }
