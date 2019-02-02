57236f99a3510f7c77ab7d4bc09b7dad54aa7248
Timothy Ji <JiTimothy@outlook.com>
2019-02-01 22:58:26 -0500

    more work done

--- Okroma/TileEngine/Map.cs
+++ Okroma/TileEngine/Map.cs
@@ -1,7 +1,23 @@
-﻿namespace Okroma.TileEngine
+﻿using Microsoft.Xna.Framework;
+
+namespace Okroma.TileEngine
 {
     public class Map
     {
+        public Point Dimensions { get; set; }
+
+        MapLayer _backgroundTiles;
+        MapLayer _tiles;
+
+        public Map(MapLayer backgroundTiles, MapLayer tiles)
+        {
+
+        }
+    }
+
+    public class MapLayer
+    {
+        Tile[,,] _tileArray;
 
     }
 }
--- Okroma/TileEngine/Tile.cs
+++ Okroma/TileEngine/Tile.cs
@@ -1,21 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using System;
 
 namespace Okroma.TileEngine
 {
     public struct Tile : IEquatable<Tile>
     {
-        public ushort Id { get; }
-        public byte Variant { get; }
-
-        public Texture2D Texture { get; }
-        private Dictionary<byte, Point> _texturePositions;
+        public int Id { get; }
 
         public void Draw(SpriteBatch spriteBatch, Vector2 drawPosition, byte variant, Color color)
         {
-            spriteBatch.Draw(Texture, drawPosition, new Rectangle(_texturePositions[variant], GameScale.TileSizePoint), color);
+            //spriteBatch.Draw(Texture, drawPosition, new Rectangle(_texturePositions[variant], GameScale.TileSizePoint), color);
         }
 
         public override bool Equals(object obj)
@@ -25,16 +20,12 @@ namespace Okroma.TileEngine
 
         public bool Equals(Tile other)
         {
-            return Id == other.Id &&
-                   Variant == other.Variant;
+            return Id == other.Id;
         }
 
         public override int GetHashCode()
         {
-            var hashCode = 31871270;
-            hashCode = hashCode * -1521134295 + Id.GetHashCode();
-            hashCode = hashCode * -1521134295 + Variant.GetHashCode();
-            return hashCode;
+            return Id;
         }
     }
 }
